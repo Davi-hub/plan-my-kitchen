1dcd911a6f3975ce45092837314c2742c3c6fabd
Davi-hub <53444813+Davi-hub@users.noreply.github.com>
2023-04-06 13:10:25 +0200

    8548453151

--- src/app/square-room/square-room-resize/square-room-resize.component.ts
+++ src/app/square-room/square-room-resize/square-room-resize.component.ts
@@ -15,58 +15,49 @@ export class SquareRoomResizeComponent implements OnInit {
   isDraggingPoint = false;
   private mouseMoveListener: (() => void) | undefined;
   private isDragging = false;
-  private startX = 0;
-  private startY = 0;
 
   constructor(private renderer: Renderer2, public squareRoomService: SquareRoomService) { }
 
   ngOnInit(): void {
-  }
-
-  startDragging(clientX: number, clientY: number) {
     this.svgEl = this.svgElRef.nativeElement;
-    this.isDragging = true;
-    this.startX = clientX - this.svgEl.parentElement!.parentElement!.getBoundingClientRect().x;
-    this.startY = clientY - this.svgEl.parentElement!.parentElement!.getBoundingClientRect().y;
-    console.log('mouseDown');
-  }
-
-  stopDragging() {
-    this.isDragging = false;
-    if (this.mouseMoveListener) {
-      this.mouseMoveListener();
-    }
-  }
-
-  activateMoveListener(moveType: string, point: paper.Point) {
-    this.mouseMoveListener = this.renderer.listen(window, moveType, (event) => {
-      if (this.isDragging) {
-        console.log('touchstart');
-        this.svgElArr = [
-          this.svgEl.parentElement!.parentElement!.getBoundingClientRect().x,
-          this.svgEl.parentElement!.parentElement!.getBoundingClientRect().y
-        ];
-        point.x = event.pageX-this.svgElArr[0];
-        point.y = event.pageY-this.svgElArr[1];
-        this.squareRoomService.reDrawSubject.next("");
-      }
-    });
   }
 
   onMouseDown(event: MouseEvent, point: paper.Point) {
     event.preventDefault();
-    this.startDragging(event.clientX, event.clientY);
+    this.isDragging = true;
     this.activateMoveListener('mousemove', point);
-
   }
 
   onTouchStart(event: TouchEvent, point: paper.Point) {
-      event.preventDefault();
-      const touch = event.touches[0];
-      this.startDragging(touch.clientX, touch.clientY);
-      this.activateMoveListener('touchmove', point);
+    event.preventDefault();
+    this.isDragging = true;
+    this.activateMoveListener('touchmove', point);
   }
 
+  activateMoveListener(moveType: string, point: paper.Point) {
+    console.log(point);
+    this.mouseMoveListener = this.renderer.listen(window, moveType, (event) => {
+      if (this.isDragging) {
+        this.svgElArr = [
+          this.svgEl.parentElement!.parentElement!.getBoundingClientRect().x,
+          this.svgEl.parentElement!.parentElement!.getBoundingClientRect().y
+        ];
+        console.log(this.svgElArr);
+
+        if (moveType == 'mousemove') {
+          console.log(event);
+          console.log(this.svgElArr);
+          point.x = event.pageX-this.svgElArr[0];
+          point.y = event.pageY-this.svgElArr[1];
+        } else {
+          const touch = event.touches[0];
+          point.x = touch.pageX-this.svgElArr[0];
+          point.y = touch.pageY-this.svgElArr[1];
+        }
+        this.squareRoomService.reDrawSubject.next("");
+      }
+    });
+  }
 
   onMouseUp() {
     this.stopDragging();
@@ -76,33 +67,10 @@ export class SquareRoomResizeComponent implements OnInit {
     this.stopDragging();
   }
 
-  // @HostListener('document:pointerup', ['$event'])
-  // public upHandle(event: PointerEvent) {
-  //   this.isDraggingPoint = false;
-  //   this.draggingPoint = null;
-  // }
-
-  // @HostListener('document:pointermove', ['$event'])
-  // public moveHandle(pointerEvent: PointerEvent) {
-  //   pointerEvent.preventDefault();
-  //   pointerEvent.stopPropagation();
-
-  //   if (this.isDraggingPoint) {
-  //     this.svgEl = this.svgElRef.nativeElement;
-  //     this.svgElArr = [
-  //       this.svgEl.parentElement!.parentElement!.getBoundingClientRect().x,
-  //       this.svgEl.parentElement!.parentElement!.getBoundingClientRect().y
-  //     ];
-  //     this.draggingPoint!.x = pointerEvent.pageX-this.svgElArr[0];
-  //     this.draggingPoint!.y = pointerEvent.pageY-this.svgElArr[1];
-  //     this.squareRoomService.reDrawSubject.next("");
-  //   }
-  // }
-
-  // downHandlePoints(pointerEvent: PointerEvent, point: paper.Point) {
-  //   this.isDraggingPoint = true;
-  //   this.draggingPoint = point;
-  //   pointerEvent.preventDefault();
-  // }
-
+  stopDragging() {
+    this.isDragging = false;
+    if (this.mouseMoveListener) {
+      this.mouseMoveListener();
+    }
+  }
 }
