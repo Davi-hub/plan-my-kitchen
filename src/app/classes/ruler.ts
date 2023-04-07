7800fd9a3fdf6125238ae6020fc0b2b03b54f055
Davi-hub <53444813+Davi-hub@users.noreply.github.com>
2023-04-07 13:39:18 +0200

    51654854151

--- src/app/classes/ruler.ts
+++ src/app/classes/ruler.ts
@@ -4,11 +4,12 @@ export class Ruler {
   inputPositionPoint;
 
   constructor(private baseLine: Line) {
-    this.inputPositionPoint = this.getInputVector(35, 25).point;
+    this.inputPositionPoint = this.getInputVector(55, 25).point;
   }
 
   drawRuler(width: number){
-    return this.drawHalfRuler(this.baseLine.P1, width)+' '+this.drawHalfRuler(this.baseLine.P2, width)+' Z';
+    // return this.drawHalfRuler(this.baseLine.P1, width)+' '+this.drawHalfRuler(this.baseLine.P2, width)+' Z';
+    return this.drawWholeRuler(this.baseLine.P1, width)+' Z';
   }
 
   drawHalfRuler(p: paper.Point, width: number) {
@@ -30,6 +31,26 @@ export class Ruler {
     let p4: paper.Point = p2.clone().add(vector4);
     let ruler = 'M' + p2.x + ' ' + p2.y + ' L' + p1.x + ' ' + p1.y + ' L' + p3.x + ' ' + p3.y +
     'M' + p2.x + ' ' + p2.y + ' L' + p4.x + ' ' + p4.y;
+    return ruler;
+  }
+
+  drawWholeRuler(p: paper.Point, width: number) {
+    let vector1 = this.baseLine.n.multiply(20);
+    let vector2 = this.baseLine.n.multiply(20);
+    let vector3 = this.baseLine.n.multiply(20);
+    let vector4 = this.baseLine.v.multiply(20);
+    vector1.length = width;
+    vector2.length = width + 5;
+    vector3.length = width + 10;
+    vector4.length = (this.baseLine.length);
+    let p1: paper.Point = p.clone().add(vector1);
+    let p2: paper.Point = p.clone().add(vector2);
+    let p3: paper.Point = p.clone().add(vector3);
+    let p4: paper.Point = p2.clone().add(vector4);
+    let p5: paper.Point = p4.clone().subtract(vector1.clone().normalize().multiply(5));
+    let p6: paper.Point = p4.clone().add(vector2.clone().normalize().multiply(5));
+    let ruler = 'M' + p2.x + ' ' + p2.y + ' L' + p1.x + ' ' + p1.y + ' L' + p3.x + ' ' + p3.y +
+    'M' + p2.x + ' ' + p2.y + ' L' + p4.x + ' ' + p4.y + ' M' + p5.x + ' ' + p5.y + ' L' + p6.x + ' ' + p6.y;
     return ruler;
   }
 
--- src/app/room/rulers/rulers.component.ts
+++ src/app/room/rulers/rulers.component.ts
@@ -7,29 +7,37 @@ import { RoomService } from '../room.service';
   templateUrl: './rulers.component.html',
   styleUrls: ['./rulers.component.css']
 })
-export class RulersComponent implements AfterViewInit{
+export class RulersComponent implements AfterViewInit {
   @Input() walls!: Wall[];
+  inputAngle!: number;
 
   constructor(public roomService: RoomService, private cdr: ChangeDetectorRef) { }
 
   ngAfterViewInit(): void {
-      this.cdr.detectChanges();
+    this.cdr.detectChanges();
   }
 
-  onClickInput(wall: Wall) {
+  onClickInput(wall: Wall, forObj: HTMLElement) {
     wall.isInputMode = true;
+    console.log(forObj);
+    forObj.setAttribute(
+      "transform",
+      "rotate(0deg, wall.ruler.inputPositionPoint.x, wall.ruler.inputPositionPoint.y)"
+    );
   }
 
-  onBlurInput(wall: Wall) {
-    wall.isInputMode = false;
+  onBlurInput() {
+    this.onClose()
   }
 
-  onClose(wall: Wall) {
-    wall.isInputMode = false;
+  onClose() {
+    this.roomService.reDrawSubject.next("");
   }
 
   onDone(i: number, input: HTMLInputElement) {
-    let choosenPoint = this.roomService.pointArray[i-1];
+    let x = i - 1;
+    if (x < 0) x = this.walls.length - 1;
+    let choosenPoint = this.roomService.pointArray[x];
     this.roomService.pointArray[i] = choosenPoint.clone()
       .add(this.walls[i].innerLine.v.normalize().multiply(+input.value));
     this.roomService.reDrawSubject.next("");
